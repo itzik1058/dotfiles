59848c6f4d76d792551b1946fec13c13719575aa
Itzik <74200683+itzik1058@users.noreply.github.com>
2024-03-16 14:45:34 +0200

    add gvfs labels for disk

--- hosts/cygnus/hardware-configuration.nix
+++ hosts/cygnus/hardware-configuration.nix
@@ -28,6 +28,7 @@
   fileSystems."/mnt/hdd" = {
     device = "/dev/disk/by-uuid/0cf4f70b-cbf3-45cf-a001-fff9c2c90a82";
     fsType = "ext4";
+    options = [ "defaults" "x-gvfs-show" "x-gvfs-name=HDD" ];
   };
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
