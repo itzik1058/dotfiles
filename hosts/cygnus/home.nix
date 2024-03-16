cae084dbb24081d58648009d76a2147485125c35
Itzik <74200683+itzik1058@users.noreply.github.com>
2024-03-16 17:21:59 +0200

    organize home config

--- hosts/cygnus/home.nix
+++ hosts/cygnus/home.nix
@@ -1,22 +1,13 @@
 { config, pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
+  home.stateVersion = "23.11";
+
+  programs.home-manager.enable = true;
+
   home.username = "koi";
   home.homeDirectory = "/home/koi";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "23.11"; # Please read the comment before changing.
-
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
   home.packages = with pkgs; [
     firefox
     tree
@@ -26,8 +17,6 @@
     gnome.gnome-software
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
   home.file = {
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
@@ -41,25 +30,7 @@
     # '';
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. If you don't want to manage your shell through Home
-  # Manager then you have to manually source 'hm-session-vars.sh' located at
-  # either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/koi/etc/profile.d/hm-session-vars.sh
-  #
   home.sessionVariables = { EDITOR = "vim"; };
-
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
 
   imports = [
     ../../modules/home-manager/gnome.nix
