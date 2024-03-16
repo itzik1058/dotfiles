b1c365379d1248eb1f4f2b957387555fe81ac6e9
Itzik <74200683+itzik1058@users.noreply.github.com>
2024-03-16 12:10:15 +0200

    define languages for gnome

--- modules/gnome.nix
+++ modules/gnome.nix
@@ -1,5 +1,7 @@
 { ... }: {
-  services.xserver.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver = {
+    enable = true;
+    displayManager.gdm.enable = true;
+    desktopManager.gnome.enable = true;
+  };
 }
--- modules/home-manager/gnome.nix
+++ modules/home-manager/gnome.nix
@@ -1,1 +1,17 @@
-{ ... }: { }
+{ lib, ... }: {
+  dconf = {
+    enable = true;
+    settings = let inherit (lib.hm.gvariant) mkTuple;
+    in {
+      "org/gnome/desktop/interface".color-scheme = "prefer-dark";
+      "org/gnome/desktop/input-sources" = {
+        sources = [ (mkTuple [ "xkb" "us" ]) (mkTuple [ "xkb" "il" ]) ];
+        xkb-options = [ "caps:escape" "grp:alt_shift_toggle" ];
+      };
+      "org/gnome/desktop/wm/keybindings" = {
+        switch-input-source = [ "<Shift>Alt_L" ];
+        switch-input-source-backward = [ "<Alt>Shift_L" ];
+      };
+    };
+  };
+}
