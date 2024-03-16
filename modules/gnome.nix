c17c354778cc8ca1ad01dd2655cd8387aedaf671
Itzik <74200683+itzik1058@users.noreply.github.com>
2024-03-16 09:35:10 +0200

    gnome disable auto suspend

--- modules/gnome.nix
+++ modules/gnome.nix
@@ -1,5 +1,19 @@
 { ... }: {
   services.xserver.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.displayManager.gdm = {
+    enable = true;
+    autoSuspend = false;
+  };
   services.xserver.desktopManager.gnome.enable = true;
+  security.polkit.extraConfig = ''
+    polkit.addRule(function(action, subject) {
+        if (action.id == "org.freedesktop.login1.suspend" ||
+            action.id == "org.freedesktop.login1.suspend-multiple-sessions" ||
+            action.id == "org.freedesktop.login1.hibernate" ||
+            action.id == "org.freedesktop.login1.hibernate-multiple-sessions")
+        {
+            return polkit.Result.NO;
+        }
+    });
+  '';
 }
