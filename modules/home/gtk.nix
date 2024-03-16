613ad8db7df3293947ca2435d1a56c28a28f21bb
Itzik <74200683+itzik1058@users.noreply.github.com>
2024-03-16 17:53:53 +0200

    fix gtk theme

--- modules/home/gtk.nix
+++ modules/home/gtk.nix
@@ -14,17 +14,6 @@
       package = pkgs.papirus-icon-theme;
       name = "Papirus";
     };
-
-    gtk3.extraConfig = {
-      Settings = ''
-        gtk-application-prefer-dark-theme=1
-      '';
-    };
-    gtk4.extraConfig = {
-      Settings = ''
-        gtk-application-prefer-dark-theme=1
-      '';
-    };
   };
 
   # GTK4
