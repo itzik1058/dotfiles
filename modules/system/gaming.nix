b18ca6b9fee70e2499936029821137050222204a
Itzik <74200683+itzik1058@users.noreply.github.com>
2024-03-16 18:34:34 +0200

    gamescope and gamemode config

--- modules/system/gaming.nix
+++ modules/system/gaming.nix
@@ -1,9 +1,17 @@
 { pkgs, ... }: {
   environment.systemPackages = with pkgs; [ heroic protonup-qt ];
+
   programs.steam = {
     enable = true;
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = true;
     gamescopeSession.enable = true;
   };
+
+  programs.gamescope = {
+    enable = true;
+    capSysNice = true;
+  };
+
+  programs.gamemode.enable = true;
 }
