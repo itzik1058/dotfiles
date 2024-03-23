31ac65575c5b66ea96d97cac19b589dc9f7844fb
Itzik <74200683+itzik1058@users.noreply.github.com>
2024-03-23 21:17:15 +0200

    garbage collection and optimiser

--- hosts/cygnus/default.nix
+++ hosts/cygnus/default.nix
@@ -3,7 +3,16 @@
 {
   system.stateVersion = "23.11";
 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix = {
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 30d";
+      persistent = true;
+    };
+    optimise.automatic = true;
+    settings.experimental-features = [ "nix-command" "flakes" ];
+  };
 
   nixpkgs.config.allowUnfree = true;
 
