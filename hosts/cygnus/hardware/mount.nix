a11dde2ffcfbf36e6032a75ff61a7c70be62f8c0
Itzik <74200683+itzik1058@users.noreply.github.com>
2024-03-28 18:00:59 +0200

    nvme mount

--- hosts/cygnus/hardware/mount.nix
+++ hosts/cygnus/hardware/mount.nix
@@ -1,7 +1,28 @@
 { ... }: {
+  boot.supportedFilesystems = [ "ntfs" ];
+
   fileSystems."/mnt/hdd" = {
     device = "/dev/disk/by-uuid/0cf4f70b-cbf3-45cf-a001-fff9c2c90a82";
     fsType = "ext4";
     options = [ "defaults" "x-gvfs-show" "x-gvfs-name=HDD" ];
   };
+
+  fileSystems."/mnt/nvme" = {
+    device = "/dev/disk/by-uuid/576bcc42-2fb5-4dfe-8efb-16c75363c7c8";
+    fsType = "ext4";
+    options = [ "defaults" "x-gvfs-show" "x-gvfs-name=NVME" ];
+  };
+
+  # fileSystems."/mnt/nvme" = {
+  #   device = "/dev/disk/by-uuid/3F5BEF1862766D0D";
+  #   fsType = "ntfs";
+  #   options = [
+  #     "noauto"
+  #     "rw"
+  #     "uid=1000"
+  #     "x-gvfs-show"
+  #     "x-gvfs-name=NVME (NTFS)"
+  #     "x-udisks-auth"
+  #   ];
+  # };
 }
