1a49e0c1f4be0b4158d1272a8d90c5cf2571dff3
Itzik <74200683+itzik1058@users.noreply.github.com>
2024-04-01 21:52:31 +0300

    update mangohud config

--- profiles/home/gaming/default.nix
+++ profiles/home/gaming/default.nix
@@ -2,5 +2,10 @@
   programs.mangohud = {
     enable = true;
     enableSessionWide = true;
+    settings = {
+      toggle_hud = "Shift_L+F11";
+      no_display = true;
+      full = true;
+    };
   };
 }
