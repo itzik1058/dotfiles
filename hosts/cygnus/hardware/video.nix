5284c54b4c34b2f8479cbaf622ebeb20d7016a6b
Itzik <74200683+itzik1058@users.noreply.github.com>
2024-04-08 13:20:45 +0300

    disable i965 vaapi

--- hosts/cygnus/hardware/video.nix
+++ hosts/cygnus/hardware/video.nix
@@ -26,7 +26,7 @@
       driSupport32Bit = true;
       extraPackages = with pkgs; [
         intel-media-driver # LIBVA_DRIVER_NAME=iHD
-        intel-vaapi-driver # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+        # intel-vaapi-driver # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
         vaapiVdpau
         libvdpau-va-gl
       ];
