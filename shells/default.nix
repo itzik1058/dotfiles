58cdc2d087443a10567f469402e9427b2f7ee984
Itzik <74200683+itzik1058@users.noreply.github.com>
2024-05-22 12:02:51 +0300

    generic python devshell for x86_64 and aarch64

--- shells/default.nix
+++ shells/default.nix
@@ -1,10 +1,1 @@
-{ nixpkgs }:
-{
-  x86_64-linux =
-    let
-      pkgs = import nixpkgs { system = "x86_64-linux"; };
-    in
-    {
-      machine-learning = pkgs.mkShell (import ./machine-learning.nix { inherit pkgs; });
-    };
-}
+{ nixpkgs }: import ./python.nix { inherit nixpkgs; }
---
+++ shells/python.nix
@@ -0,0 +1,33 @@
+{ nixpkgs }:
+let
+  supportedSystems = [
+    "x86_64-linux"
+    "x86_64-darwin"
+    "aarch64-linux"
+    "aarch64-darwin"
+  ];
+in
+nixpkgs.lib.genAttrs supportedSystems (
+  system:
+  let
+    pkgs = import nixpkgs { system = system; };
+  in
+  {
+    python = pkgs.mkShell {
+      packages = with pkgs; [
+        ffmpeg
+        (python3.withPackages (
+          python-pkgs: with python-pkgs; [
+            pip
+            numpy
+            pandas
+            scikit-learn
+            matplotlib
+            plotly
+            jupyterlab
+          ]
+        ))
+      ];
+    };
+  }
+)
