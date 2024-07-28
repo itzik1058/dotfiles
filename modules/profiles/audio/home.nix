6a875eb43fb33348be712d8af16340450931e253
Itzik <74200683+itzik1058@users.noreply.github.com>
2024-07-28 22:17:52 +0300

    easyeffects remove deepfilternet

--- modules/profiles/audio/home.nix
+++ modules/profiles/audio/home.nix
@@ -25,12 +25,7 @@ in
       settings = {
         "com/github/wwmm/easyeffects/streaminputs".plugins = [
           "rnnoise#0"
-          "deepfilternet#0"
         ];
-
-        "com/github/wwmm/easyeffects/streaminputs/deepfilternet/0" = {
-          bypass = false;
-        };
 
         "com/github/wwmm/easyeffects/streaminputs/rnnoise/0" = {
           bypass = false;
