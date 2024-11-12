37f085137ee607e6f9c8aef9c4f0dbb56f32cfa5
Itzik <74200683+itzik1058@users.noreply.github.com>
2024-11-12 16:40:04 +0200

    nvim cmp mapping

--- modules/nixvim/plugins/cmp.nix
+++ modules/nixvim/plugins/cmp.nix
@@ -35,13 +35,48 @@
           }
         ];
         mapping = {
-          "<C-n>" = "cmp.mapping.select_next_item()";
-          "<C-p>" = "cmp.mapping.select_prev_item()";
-          "<C-e>" = "cmp.mapping.abort()";
-          "<C-u>" = "cmp.mapping.scroll_docs(-4)";
-          "<C-d>" = "cmp.mapping.scroll_docs(4)";
-          "<C-Space>" = "cmp.mapping.complete()";
-          "<CR>" = "cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true })";
+          "<CR>" = ''
+            cmp.mapping(function(fallback)
+              local luasnip = require("luasnip")
+              if cmp.visible() then
+                if luasnip.expandable() then
+                  luasnip.expand()
+                else
+                  cmp.confirm({
+                      select = true,
+                  })
+                end
+              else
+                fallback()
+              end
+            end)
+          '';
+          "<Tab>" = ''
+            cmp.mapping(function(fallback)
+              local luasnip = require("luasnip")
+              if cmp.visible() then
+                cmp.select_next_item()
+              elseif luasnip.locally_jumpable(1) then
+                luasnip.jump(1)
+              else
+                fallback()
+              end
+            end, { "i", "s" })
+          '';
+          "<S-Tab>" = ''
+            cmp.mapping(function(fallback)
+              local luasnip = require("luasnip")
+              if cmp.visible() then
+                cmp.select_prev_item()
+              elseif luasnip.locally_jumpable(-1) then
+                luasnip.jump(-1)
+              else
+                fallback()
+              end
+            end, { "i", "s" })
+          '';
+          "j" = "cmp.mapping.scroll_docs(1)";
+          "k" = "cmp.mapping.scroll_docs(-1)";
         };
       };
     };
