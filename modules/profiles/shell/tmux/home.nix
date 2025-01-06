e446cca9c2697843f8c59b3f800afc8e04b0bcf2
Itzik <74200683+itzik1058@users.noreply.github.com>
2025-01-06 17:13:26 +0200

    fix tmux resurrect nvim

--- modules/profiles/shell/tmux/home.nix
+++ modules/profiles/shell/tmux/home.nix
@@ -28,7 +28,7 @@ in
           extraConfig = ''
             resurrect_dir="$HOME/.tmux/resurrect"
             set -g @resurrect-dir $resurrect_dir
-            set -g @resurrect-hook-post-save-all "sed -i 's/--cmd lua.*--cmd set packpath/--cmd \"lua/g; s/--cmd set rtp.*\$/\"/' $resurrect_dir/last"
+            set -g @resurrect-hook-post-save-all "sed -i -E 's|:/nix/store/[^ ]+/bin/nvim.*|:nvim|g' $resurrect_dir/last"
             set -g @resurrect-capture-pane-contents 'on'
             set -g @resurrect-processes '"~nvim"'
           '';
