5ace7532ae298b4bad39b004bed10dd8e30e722b
Itzik <74200683+itzik1058@users.noreply.github.com>
2025-01-16 11:35:31 +0200

    swaync widgets

--- modules/profiles/desktop/swaync/home.nix
+++ modules/profiles/desktop/swaync/home.nix
@@ -15,6 +15,34 @@ in
     services.swaync = {
       enable = true;
       style = builtins.readFile ./style.css;
+      settings = {
+        widgets = [
+          "title"
+          "dnd"
+          "backlight"
+          "volume"
+          "mpris"
+          "notifications"
+        ];
+        widget-config = {
+          title = {
+            text = "Notifications";
+            clear-all-buttons = true;
+            button-text = "Clear All";
+          };
+          dnd.text = "Do Not Disturb";
+          backlight = {
+            label = "󰃞";
+            device = "intel_backlight";
+            min = 10;
+          };
+          volume.label = "";
+          mpris = {
+            image-size = 96;
+            image-radius = 12;
+          };
+        };
+      };
     };
   };
 }
