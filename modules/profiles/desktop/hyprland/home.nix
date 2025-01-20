2f3a4117c6e50014f94fe168a0003cb83275b9fb
Itzik <74200683+itzik1058@users.noreply.github.com>
2025-01-20 16:39:52 +0200

    hyprland reformat

--- modules/profiles/desktop/hyprland/home.nix
+++ modules/profiles/desktop/hyprland/home.nix
@@ -68,104 +68,76 @@ in
           cliphist = lib.getExe config.services.cliphist.package;
           hyprshot = lib.getExe pkgs.hyprshot;
           playerctl = lib.getExe pkgs.playerctl;
-          swayosd-client = lib.getExe' config.services.swayosd.package "swayosd-client";
           swaync-client = lib.getExe' pkgs.swaynotificationcenter "swaync-client";
+          swayosd-client = lib.getExe' config.services.swayosd.package "swayosd-client";
           terminal = lib.getExe config.profiles.terminal.package;
           tmux = lib.getExe config.programs.tmux.package;
           tofi-drun = lib.getExe' config.programs.tofi.package "tofi-drun";
           wl-copy = lib.getExe' pkgs.wl-clipboard "wl-copy";
         in
         {
-          general = {
-            gaps_out = 10;
-          };
-          input = {
-            kb_layout = "us,il";
-            kb_options = [ "grp:alt_shift_toggle" ];
-            touchpad = {
-              natural_scroll = true;
-              disable_while_typing = true;
-            };
-          };
           animations.animation = [
             "specialWorkspace, 1, 4, default, slidevert"
           ];
-
-          "$mod" = "SUPER";
           bind = [
-            "$mod, grave, hyprexpo:expo, toggle"
-            "$mod, Q, killactive,"
-            "$mod, M, togglefloating"
-            "$mod, Z, fullscreen"
-            "$mod, V, exec, ${cliphist} list | tofi | ${cliphist} decode | ${wl-copy}"
-            "$mod, N, exec, ${swaync-client} -t"
-            "$mod, Return, exec, ${tofi-drun} | xargs hyprctl dispatch exec --"
-            "$mod ALT, L, exec, loginctl lock-session"
+            "SUPER, grave, hyprexpo:expo, toggle"
+            "SUPER, Q, killactive,"
+            "SUPER, M, togglefloating"
+            "SUPER, Z, fullscreen"
+            "SUPER, V, exec, ${cliphist} list | tofi | ${cliphist} decode | ${wl-copy}"
+            "SUPER, N, exec, ${swaync-client} -t"
+            "SUPER, Return, exec, ${tofi-drun} | xargs hyprctl dispatch exec --"
+            "SUPER ALT, L, exec, loginctl lock-session"
 
-            "$mod, h, movefocus, l"
-            "$mod, l, movefocus, r"
-            "$mod, k, movefocus, u"
-            "$mod, j, movefocus, d"
-            "$mod, left, movefocus, l"
-            "$mod, right, movefocus, r"
-            "$mod, up, movefocus, u"
-            "$mod, down, movefocus, d"
+            "SUPER, h, movefocus, l"
+            "SUPER, l, movefocus, r"
+            "SUPER, k, movefocus, u"
+            "SUPER, j, movefocus, d"
+            "SUPER, left, movefocus, l"
+            "SUPER, right, movefocus, r"
+            "SUPER, up, movefocus, u"
+            "SUPER, down, movefocus, d"
 
-            "$mod SHIFT, h, movewindow, l"
-            "$mod SHIFT, l, movewindow, r"
-            "$mod SHIFT, k, movewindow, u"
-            "$mod SHIFT, j, movewindow, d"
-            "$mod SHIFT, left, movewindow, l"
-            "$mod SHIFT, right, movewindow, r"
-            "$mod SHIFT, up, movewindow, u"
-            "$mod SHIFT, down, movewindow, d"
+            "SUPER SHIFT, h, movewindow, l"
+            "SUPER SHIFT, l, movewindow, r"
+            "SUPER SHIFT, k, movewindow, u"
+            "SUPER SHIFT, j, movewindow, d"
+            "SUPER SHIFT, left, movewindow, l"
+            "SUPER SHIFT, right, movewindow, r"
+            "SUPER SHIFT, up, movewindow, u"
+            "SUPER SHIFT, down, movewindow, d"
 
             "ALT, Tab, cyclenext"
             "ALT, Tab, bringactivetotop"
 
-            "$mod, 1, workspace, 1"
-            "$mod, 2, workspace, 2"
-            "$mod, 3, workspace, 3"
-            "$mod, 4, workspace, 4"
-            "$mod, 5, workspace, 5"
-            "$mod, 6, workspace, 6"
-            "$mod, 7, workspace, 7"
-            "$mod, 8, workspace, 8"
-            "$mod, 9, workspace, 9"
-            "$mod, 0, workspace, 0"
-            "$mod SHIFT, 1, movetoworkspace, 1"
-            "$mod SHIFT, 2, movetoworkspace, 2"
-            "$mod SHIFT, 3, movetoworkspace, 3"
-            "$mod SHIFT, 4, movetoworkspace, 4"
-            "$mod SHIFT, 5, movetoworkspace, 5"
-            "$mod SHIFT, 6, movetoworkspace, 6"
-            "$mod SHIFT, 7, movetoworkspace, 7"
-            "$mod SHIFT, 8, movetoworkspace, 8"
-            "$mod SHIFT, 9, movetoworkspace, 9"
-            "$mod SHIFT, 0, movetoworkspace, 0"
-            "$mod, Tab, workspace, e+1"
-            "$mod SHIFT, Tab, workspace, e-1"
+            "SUPER, 1, workspace, 1"
+            "SUPER, 2, workspace, 2"
+            "SUPER, 3, workspace, 3"
+            "SUPER, 4, workspace, 4"
+            "SUPER, 5, workspace, 5"
+            "SUPER, 6, workspace, 6"
+            "SUPER, 7, workspace, 7"
+            "SUPER, 8, workspace, 8"
+            "SUPER, 9, workspace, 9"
+            "SUPER, 0, workspace, 0"
+            "SUPER SHIFT, 1, movetoworkspace, 1"
+            "SUPER SHIFT, 2, movetoworkspace, 2"
+            "SUPER SHIFT, 3, movetoworkspace, 3"
+            "SUPER SHIFT, 4, movetoworkspace, 4"
+            "SUPER SHIFT, 5, movetoworkspace, 5"
+            "SUPER SHIFT, 6, movetoworkspace, 6"
+            "SUPER SHIFT, 7, movetoworkspace, 7"
+            "SUPER SHIFT, 8, movetoworkspace, 8"
+            "SUPER SHIFT, 9, movetoworkspace, 9"
+            "SUPER SHIFT, 0, movetoworkspace, 0"
+            "SUPER, Tab, workspace, e+1"
+            "SUPER SHIFT, Tab, workspace, e-1"
 
             ", F12, toggleSpecialWorkspace, terminal"
 
             ", PRINT, exec, ${hyprshot} -m region"
             "SHIFT, PRINT, exec, ${hyprshot} -m output"
-            "$mod, PRINT, exec, ${hyprshot} -m window"
-          ];
-          bindm = [
-            "$mod, mouse:272, movewindow"
-            "$mod, mouse:273, resizewindow"
-            "$mod ALT, mouse:272, resizewindow"
-          ];
-          bindr = [
-            "SUPER, SUPER_L, hyprexpo:expo, toggle"
-            "CAPS, Caps_Lock, exec, ${swayosd-client} --caps-lock"
-          ];
-          bindl = [
-            ",XF86AudioPlay, exec, ${playerctl} play-pause"
-            ",XF86AudioPause, exec, ${playerctl} play-pause"
-            ",XF86AudioNext, exec, ${playerctl} next"
-            ",XF86AudioPrev, exec, ${playerctl} previous"
+            "SUPER, PRINT, exec, ${hyprshot} -m window"
           ];
           bindel = [
             ",XF86AudioLowerVolume, exec, ${swayosd-client} --output-volume lower --max-volume 100"
@@ -175,17 +147,39 @@ in
             ",XF86MonBrightnessDown, exec, ${swayosd-client} --brightness lower"
             ",XF86MonBrightnessUp, exec, ${swayosd-client} --brightness raise"
           ];
-
-          workspace = [
-            "special:terminal, on-created-empty:${terminal} -e ${tmux} new-session -As0"
+          bindl = [
+            ",XF86AudioPlay, exec, ${playerctl} play-pause"
+            ",XF86AudioPause, exec, ${playerctl} play-pause"
+            ",XF86AudioNext, exec, ${playerctl} next"
+            ",XF86AudioPrev, exec, ${playerctl} previous"
           ];
-
+          bindm = [
+            "SUPER, mouse:272, movewindow"
+            "SUPER, mouse:273, resizewindow"
+            "SUPER ALT, mouse:272, resizewindow"
+          ];
+          bindr = [
+            "SUPER, SUPER_L, hyprexpo:expo, toggle"
+            "CAPS, Caps_Lock, exec, ${swayosd-client} --caps-lock"
+          ];
+          general.gaps_out = 10;
+          input = {
+            kb_layout = "us,il";
+            kb_options = [ "grp:alt_shift_toggle" ];
+            touchpad = {
+              natural_scroll = true;
+              disable_while_typing = true;
+            };
+          };
           windowrulev2 = [
             "float, class:nm-connection-editor"
             "float, class:\\.blueman.*"
             "float, class:org.pulseaudio.pavucontrol"
 
             "noblur, class:com.mitchellh.ghostty"
+          ];
+          workspace = [
+            "special:terminal, on-created-empty:${terminal} -e ${tmux} new-session -As0"
           ];
         };
     };
