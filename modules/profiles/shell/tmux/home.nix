4bcd611c0ae2622f3da56bd5a9cde2bc71ff377a
Itzik <74200683+itzik1058@users.noreply.github.com>
2025-06-04 20:11:57 +0300

    fzf tmux integration

--- modules/profiles/shell/tmux/home.nix
+++ modules/profiles/shell/tmux/home.nix
@@ -13,29 +13,35 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    programs.tmux = {
-      enable = true;
-      prefix = "C-a";
-      keyMode = "vi";
-      mouse = true;
-      baseIndex = 1;
-      escapeTime = 10;
-      clock24 = true;
-      terminal = "tmux-256color";
-      plugins = with pkgs.tmuxPlugins; [
-        catppuccin
-        vim-tmux-navigator
-        {
-          plugin = resurrect;
-          extraConfig = ''
-            resurrect_dir="$HOME/.tmux/resurrect"
-            set -g @resurrect-dir $resurrect_dir
-            set -g @resurrect-hook-post-save-all "sed -i -E 's|:/nix/store/[^ ]+/bin/nvim.*|:nvim|g' $resurrect_dir/last"
-            set -g @resurrect-capture-pane-contents 'on'
-            set -g @resurrect-processes '"~nvim"'
-          '';
-        }
-      ];
+    programs = {
+      tmux = {
+        enable = true;
+        prefix = "C-a";
+        keyMode = "vi";
+        mouse = true;
+        baseIndex = 1;
+        escapeTime = 10;
+        clock24 = true;
+        terminal = "tmux-256color";
+        plugins = with pkgs.tmuxPlugins; [
+          catppuccin
+          vim-tmux-navigator
+          {
+            plugin = resurrect;
+            extraConfig = ''
+              resurrect_dir="$HOME/.tmux/resurrect"
+              set -g @resurrect-dir $resurrect_dir
+              set -g @resurrect-hook-post-save-all "sed -i -E 's|:/nix/store/[^ ]+/bin/nvim.*|:nvim|g' $resurrect_dir/last"
+              set -g @resurrect-capture-pane-contents 'on'
+              set -g @resurrect-processes '"~nvim"'
+            '';
+          }
+        ];
+      };
+      fzf.tmux = {
+        enableShellIntegration = true;
+        shellIntegrationOptions = [ "-p 50%" ];
+      };
     };
   };
 }
