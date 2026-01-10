cb532183f4b8579e4261e0fdd78b9b0af8475d9f
Itzik <itzik@koyfman.net>
2026-01-10 21:18:51 +0200

    ghostty bindings

--- modules/misc/terminal/ghostty.nix
+++ modules/misc/terminal/ghostty.nix
@@ -9,6 +9,20 @@
             keybind = [
               "ctrl+comma=unbind"
               "ctrl+shift+comma=unbind"
+
+              "alt+h=goto_split:left"
+              "alt+j=goto_split:down"
+              "alt+k=goto_split:up"
+              "alt+l=goto_split:right"
+
+              "ctrl+a>alt+h=resize_split:left,200"
+              "ctrl+a>alt+j=resize_split:down,200"
+              "ctrl+a>alt+k=resize_split:up,200"
+              "ctrl+a>alt+l=resize_split:right,200"
+              "ctrl+a>alt+equal=equalize_splits"
+
+              "ctrl+a>shift+quote=new_split:down"
+              "ctrl+a>shift+5=new_split:right"
             ];
           };
         };
