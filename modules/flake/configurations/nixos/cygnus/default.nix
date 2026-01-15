f095d68d2df7de2a25a233ad0efaea1dd50aa27a
Itzik <itzik@koyfman.net>
2026-01-15 19:29:46 +0200

    remove autologin workaround
    the issue has been fixed

--- modules/flake/configurations/nixos/cygnus/default.nix
+++ modules/flake/configurations/nixos/cygnus/default.nix
@@ -68,10 +68,6 @@ in
         imports = homeManagerModules;
       };
 
-      # https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
-      systemd.services."getty@tty1".enable = false;
-      systemd.services."autovt@tty1".enable = false;
-
       services = {
         displayManager.autoLogin.user = "koi";
         printing.enable = true;
