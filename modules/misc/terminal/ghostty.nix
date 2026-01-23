31e2ca722e9aa5906759aeb9fc1b99d127b8423c
Itzik <itzik@koyfman.net>
2026-01-23 11:10:25 +0200

    ghostty config

--- modules/misc/terminal/ghostty.nix
+++ modules/misc/terminal/ghostty.nix
@@ -10,19 +10,24 @@
               "ctrl+comma=unbind"
               "ctrl+shift+comma=unbind"
 
+              "alt+p=previous_tab"
+              "alt+n=next_tab"
+
+              "alt+[=goto_split:previous"
+              "alt+]=goto_split:next"
               "alt+h=goto_split:left"
               "alt+j=goto_split:down"
               "alt+k=goto_split:up"
               "alt+l=goto_split:right"
 
-              "ctrl+a>alt+h=resize_split:left,200"
-              "ctrl+a>alt+j=resize_split:down,200"
-              "ctrl+a>alt+k=resize_split:up,200"
-              "ctrl+a>alt+l=resize_split:right,200"
-              "ctrl+a>alt+equal=equalize_splits"
+              "alt+shift+h=resize_split:left,200"
+              "alt+shift+j=resize_split:down,200"
+              "alt+shift+k=resize_split:up,200"
+              "alt+shift+l=resize_split:right,200"
+              "alt+equal=equalize_splits"
 
-              "ctrl+a>shift+quote=new_split:down"
-              "ctrl+a>shift+5=new_split:right"
+              "alt+;=new_split:down"
+              "alt+'=new_split:right"
             ];
           };
         };
